21b6a2459fb96ceeb4a0a65061aab16b65ef03c7
gabriel campos <gabriel>
2023-11-02 08:11:03 -0300

    importante

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+$(document).ready(function () {
+  var quantity = 1; // Quantidade inicial
 
+  // Aumenta a quantidade
+  $('#increaseQuantity').click(function () {
+    quantity++;
+    updateQuantity();
+  });
+
+  // Diminui a quantidade, no mínimo 1
+  $('#decreaseQuantity').click(function () {
+    if (quantity > 1) {
+      quantity--;
+      updateQuantity();
+    }
+  });
+
+  // Função para atualizar a quantidade exibida
+  function updateQuantity() {
+    $('#quantity').text(quantity);
+  }
+});
